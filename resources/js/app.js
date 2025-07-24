9d3c814108de0355e9d37a8667d10c36acee6adc
Jonas Fernandes dos Santos <jonasfsantos194@gmail.com>
2025-07-24 00:06:12 -0300

    feat[RKT]: Adicionando dependencias, system colors, fontes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import $ from 'jquery';
+import 'select2/dist/css/select2.min.css';
+import 'select2';
+import '../scss/app.scss';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// JQUERY
+window.$ = $;
+window.jQuery = $;
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
-<div class="bg-gray-800 shadow-md text-white">
+<div class="header">
     <div class="container mx-auto px-4 py-4 flex justify-between items-center">
         <a href="{{ route('home') }}" class="text-2xl font-bold text-white-400">ROCKTON</a>
-        <nav class="space-x-4">
+        <nav class="space-x-4 header-fonts">
             <a href="" class="hover:text-green-400">INICIO</a>
             <a href="" class="hover:text-green-400">BANDAS</a>
             <a href="" class="hover:text-green-400">MÚSICAS</a>
