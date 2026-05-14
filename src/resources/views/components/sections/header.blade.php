fece7aae344ec2ac49a4e3661130f5d704e20b70
Jonas Fernandes dos Santos <jonasfsantos194@gmail.com>
2026-05-13 23:34:55 -0300

    [RKT]: Iniciando projeto

---
+++ src/resources/views/components/sections/header.blade.php
@@ -0,0 +1,21 @@
+<header class="w-full bg-zinc-800 text-white">
+    <div class="flex items-center justify-between px-16 py-8">
+        <a href="">
+            <img
+                src="{{ asset('assets/imagens/logos/rockton-branco.svg') }}"
+                alt="ROCKTON"
+                class="w-40"
+            >
+        </a>
+
+        <nav>
+            <ul class="flex gap-8 font-medium">
+                <li><a href="#">SOBRE</a></li>
+                <li><a href="#">BANDAS</a></li>
+                <li><a href="#">ÁLBUNS</a></li>
+                <li><a href="#">MÚSICOS</a></li>
+                <li><a href="#">MÚSICAS</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- src/vite.config.js
+++ src/vite.config.js
@@ -3,9 +3,20 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+            port: 5174,
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
