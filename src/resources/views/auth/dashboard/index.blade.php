6589e1a723d059e41372d8d2e03bae9d7f1990ff
Jonas Fernandes dos Santos <jonasfsantos194@gmail.com>
2026-06-21 23:34:57 -0300

    feat[RKT]: Add configurações de layout no dashboard

--- src/resources/views/auth/dashboard/index.blade.php
+++ src/resources/views/auth/dashboard/index.blade.php
@@ -1,11 +1,19 @@
 <x-layouts.dashboard>
-   <section
-        class="min-vh-50 bg-hero"
-        style="background-image: url('{{ asset('assets/rock-band-show.png') }}')">
-
-        <div class="d-flex align-items-center justify-content-center min-vh-50">
-            Conteúdo
+    <section class="position-relative">
+           <div class="bg-hero d-flex align-items-center"
+               style="background-image: linear-gradient(90deg, rgba(3,7,18,0.95) 0%, rgba(3,7,18,0.65) 35%, rgba(3,7,18,0.12) 60%), url('http://localhost:8000/assets/rock-band-show.png'); background-size: cover; background-position: right center; background-repeat: no-repeat; min-height: 50vh; border: 1px solid #27272a; border-radius: 10px;">
+            <div class="container text-center text-white">
+                <div class="row">
+                    <div class="col-12">
+                        <p style="text-align: left;">
+                            Bem vindo ao <strong>ROCKTON</strong><br>
+                            Sua vitrine digital para músicos, bandas e álbuns — tudo em um só lugar.<br><br>
+                            Conheça histórias, trajetórias, discografias e colaborações.<br>
+                            Por trás de cada som existe uma história, e aqui é o lugar para descobri-las.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </section>
 </x-layouts.dashboard>
